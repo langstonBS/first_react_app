58b0e22de7f1b072b6ba4f4efb76ab3e913ce2fd
langstonBS <langston.beckwithstanley@gmail.com>
2020-05-08 08:52:00 -0700

    commit the files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  NavLink,
+  Route,
+} from "react-router-dom";
 import './App.css';
+import HomePage from './HomePage';
+import ListPage from './ListPage'
+import ListItem from './ListItem';
+import AboutMePage from './AboutMePage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+      return (
+          <div>
+              <Router>
+                <Switch>
+                    <Route path="/HomePage">
+                      <HomePage />
+                    </Route>
+
+                <Route path="/AboutMePage">
+                    <AboutMePage />
+                </Route>
+
+                <Route path="/ListPage">
+                      <ListPage />
+                </Route>
+
+              <Route path="/ListItem">
+                <ListItem />
+              </Route>
+
+              </Switch> 
+              </Router>
+          </div>        
+      )
+  }
 }
 
-export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <h1> I am the home page</h1>
+            </div>
+        )
+    }
+}
